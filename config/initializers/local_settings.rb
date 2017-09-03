533f7a74db3712f1e4cdb4c64e1b038a66ca6892
William B. Hauck <wbhauck@yahoo.com>
2017-09-03 17:27:05 -0400

    do not show password_digest

--- config/initializers/local_settings.rb
+++ config/initializers/local_settings.rb
@@ -2,7 +2,7 @@ module BlogApp
   class Application
     SITE_NAV_BRAND = "WBHauck"
     SITE_TITLE = "WBHauck"
-    VERSION = "0.0.5"
+    VERSION = "0.0.6"
     FOOTER_COPYRIGHT = '2017 William B. Hauck'
   end
 end
