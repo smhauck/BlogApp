23dfadb6d9885f04322ad33812fcbe988d99d21d
William B. Hauck <wbhauck@yahoo.com>
2017-09-06 01:39:34 -0400

    add post link now available if you are logged in; updaged version number

--- config/initializers/local_settings.rb
+++ config/initializers/local_settings.rb
@@ -2,7 +2,7 @@ module BlogApp
   class Application
     SITE_NAV_BRAND = "WBHauck"
     SITE_TITLE = "WBHauck"
-    VERSION = "0.0.11"
+    VERSION = "0.0.12"
     FOOTER_COPYRIGHT = '2017 William B. Hauck'
   end
 end
