b01008cbd096c7685c72e9eea2d13d8443e2218e
William B. Hauck <wbhauck@yahoo.com>
2017-09-19 15:54:47 -0400

    removed show, edit, destroy links from tags/index. do not need to see them

--- config/initializers/local_settings.rb
+++ config/initializers/local_settings.rb
@@ -2,7 +2,7 @@ module BlogApp
   class Application
     SITE_NAV_BRAND = "WBHauck"
     SITE_TITLE = "WBHauck"
-    VERSION = "0.0.13"
+    VERSION = "0.0.14"
     FOOTER_COPYRIGHT = '2017 William B. Hauck'
   end
 end
