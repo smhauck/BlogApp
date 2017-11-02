14fefcd85c76e726454b0d8cab0387b2d8af9c80
William B. Hauck <wbhauck@yahoo.com>
2017-11-02 00:00:48 -0400

    show user-related items if logged in

--- config/initializers/local_settings.rb
+++ config/initializers/local_settings.rb
@@ -2,7 +2,7 @@ module BlogApp
   class Application
     SITE_NAV_BRAND = "WBHauck"
     SITE_TITLE = "WBHauck"
-    VERSION = "0.0.18"
+    VERSION = "0.0.19"
     FOOTER_COPYRIGHT = '2017 William B. Hauck'
   end
 end
