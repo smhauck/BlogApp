95669758cdf21f165883aa0390b9135c040ae373
William B. Hauck <wbhauck@yahoo.com>
2018-05-11 16:14:02 -0400

    fixed devise / actionmailer issue

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+
