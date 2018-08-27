2de160b5edf7b39c1e5005453c3671f3ea3a2892
William B. Hauck <wbhauck@yahoo.com>
2018-08-27 17:29:37 -0400

    updating to Bootstrap 4.1

--- config/initializers/local_settings.rb
+++ config/initializers/local_settings.rb
@@ -2,7 +2,7 @@ module BlogApp
   class Application
     SITE_NAV_BRAND = "WBHauck"
     SITE_TITLE = "WBHauck"
-    VERSION = "0.0.22"
+    VERSION = "0.0.23"
     FOOTER_COPYRIGHT = '2018 William B. Hauck'
   end
 end
